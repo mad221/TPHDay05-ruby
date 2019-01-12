6174ff21323bc40a68cfe8eb25fae95ed742b7f8
mad221 <maximepouzol@gmail.com>
2019-01-12 18:37:16 +0100

    exo twitter avec fonction courte

--- temp.rb
+++ temp.rb
@@ -1,188 +1,70 @@
 twitter=["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
 
-
-i=0
-g=0
-
-numero=0
-aude=0
-arobase=0
-
-majuscule=0
-mouchard=0
-
-underscore=0;
-
-while(twitter[i]!=nil)
-
-mots=twitter[i]
-
-while (mots[g]!=nil)
-
-print mots[g]
-if (mots[g].ord==95)
-
-underscore=underscore+1
+def journaliste(array)
+puts array.length
 
 end
 
-if (mots[g].ord>=48 && mots[g].ord<=57)
-numero=numero+1
+def handlenumero(array)
 
-end
-if (mots[g]=="a" &&mots[g+1]=="u" &&mots[g+2]=="d" &&mots[g+3]=="e")
-aude=aude+1
+puts array.grep(/\d+/).length
 
 end
 
-if (mots[g]=="A" &&mots[g+1]=="u" &&mots[g+2]=="d" &&mots[g+3]=="e")
-aude=aude+1
+def aude (array)
+
+
+puts array.grep(/Aude|aude/).length
 
 end
 
-if (mots[g]=="a" &&mots[g+1]=="U" &&mots[g+2]=="d" &&mots[g+3]=="e")
-aude=aude+1
+def beginH(array)
+
+puts array.grep(/@[A-Z]/).length
 
 end
 
-if (mots[g]=="a" &&mots[g+1]=="u" &&mots[g+2]=="D" &&mots[g+3]=="e")
-aude=aude+1
+def hight(array)
+
+puts array.grep(/[A-Z]/).length
 
 end
 
-if (mots[g]=="a" &&mots[g+1]=="u" &&mots[g+2]=="d" &&mots[g+3]=="E")
-aude=aude+1
+def underscore(array)
+
+puts array.select {|v| v =~ /[_]/}.length
 
 end
 
-if (mots[g].ord>=65 && mots[g].ord<=90)
-mouchard=mouchard+1
+def ssort(array)
+  puts array.sort
+end
+
+
+def short50(array)
+
+puts array.sort[0,50]
+
+end
+
+def epenser(array)
+
+puts array.index("@epenser")
+
 
 end
 
 
 
-g=g+1
 
-end
 
-
-if  (mouchard>0)
-
-majuscule=majuscule+1
-
-
-
-mouchard=0
-
-end
-
-puts
-
-g=0
-if (mots[0].ord==64)
-
-arobase=arobase+1
-
-
-end 
-
-i=i+1
-
-end 
-
-puts i
-
-
-puts "numero=#{numero}"
-
-puts "nombre de aude=#{aude}"
-
-puts "arobase=#{arobase}"
-
-puts "majuscule=#{majuscule}"
-
-puts "tiret du bas=#{underscore}"
-
-
-
-#puts twitter.sort
-
-
-
-
-
-
-increment=0
-ge=0
-iterateur=0
-trieur=[]
-while (twitter[increment]!=nil)
-
-
-contient=twitter[increment]
-while (contient[ge]!=nil)
-
-
-
-
-
-ge=ge+1
-
-end
-
-trieur[increment]=ge
-
-
-
-increment=increment+1
-end
-
-
-premier=trieur.sort
-
-
-
-while (iterateur<=50)
-
-
-puts premier[iterateur]
-iterateur=iterateur+1
-
-
-end
-
-
-i=0
-g=0
-
-
-while (twitter[i]!=nil)
-
-mots=twitter[i]
-
-
-
-if mots=="@epenser"
-
-puts "epenser est a la position #{i}"
-
-end
-
-i=i+1
-end 
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
+journaliste(twitter)
+handlenumero(twitter)
+aude(twitter)
+beginH(twitter)
+hight(twitter)
+underscore(twitter)
+#ssort(twitter) trop chiante tu vois plus le resultat du reste mais tkt elle marche
+#short50(twitter) la meme pour lui
+epenser(twitter)
